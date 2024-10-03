e176d676283461d0c2ad045bb405881c993adc7c
Irum Mumtaz <irummumtaz1433@gmail.com>
2024-10-04 01:17:40 +0400

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Bank_Marketing",
+    version="0.0.0",
+    author="Irum",
+    author_email="irummumtaz1433@gmail.com",
+    packages=find_packages()
+)
